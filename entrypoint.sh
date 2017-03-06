f84d5be229ae7df228bb7f1430cd1c0156e2facd
calata <carlos.calatayud@byteflair.com>
2017-03-06 16:32:49 +0100

    Check API endpoint project_status Environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 echo "Executing entrypoint"
-curl https://sonar.byteflair.com/api/projects/index
+
+
+if [ -z "$SONAR_LOGIN" ]; then
+    echo "SONAR_LOGIN NOT FOUND";
+    exit 1;
+fi
+
+if [ -z "$SONAR_PASSWORD" ]; then
+    echo "SONAR_PASSWORD NOT FOUND";
+    exit 1;
+fi
+
+if [ -z "$SONAR_HOST" ]; then
+    echo "SONAR_HOST NOT FOUND";
+    exit 1;
+fi
+
+if [ -z "$PROJECT_ORG" ]; then
+    echo "PROJECT_ORG NOT FOUND";
+    exit 1;
+fi
+
+if [ -z "$PROJECT_NAME" ]; then
+    echo "PROJECT_NAME NOT FOUND";
+    exit 1;
+fi
+
+
+response=$(curl -u $SONAR_LOGIN:$SONAR_PASSWORD $SONAR_HOST/api/qualitygates/project_status?projectKey=$PROJECT_ORG:$PROJECT_NAME | jq '.projectStatus.status')
+
+if [[ "$response" == *"ERROR"* ]]; then
+        echo "Quality gate decreased";
+	exit 1;
+fi
