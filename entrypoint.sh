baea40b78b29bdfe1ab87f6d906328a88a6dd538
Daniel Cerecedo <daniel.cerecedo@byteflair.com>
2017-03-09 11:25:12 +0100

    Usar params plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,33 +4,33 @@ set -e
 echo "Executing entrypoint"
 
 
-if [ -z "$SONAR_LOGIN" ]; then
-    echo "SONAR_LOGIN NOT FOUND";
+if [ -z "$PLUGIN_USERNAME" ]; then
+    echo "username not set!";
     exit 1;
 fi
 
-if [ -z "$SONAR_PASSWORD" ]; then
-    echo "SONAR_PASSWORD NOT FOUND";
+if [ -z "$PLUGIN_PASSWORD" ]; then
+    echo "password not set!";
     exit 1;
 fi
 
-if [ -z "$SONAR_HOST" ]; then
-    echo "SONAR_HOST NOT FOUND";
+if [ -z "$PLUGIN_HOST" ]; then
+    echo "host not set!";
     exit 1;
 fi
 
-if [ -z "$PROJECT_ORG" ]; then
-    echo "PROJECT_ORG NOT FOUND";
+if [ -z "$PLUGIN_PROJECT_ORGANIZATION" ]; then
+    echo "project_organization not set!";
     exit 1;
 fi
 
-if [ -z "$PROJECT_NAME" ]; then
-    echo "PROJECT_NAME NOT FOUND";
+if [ -z "$PLUGIN_PROJECT_NAME" ]; then
+    echo "project_name not set!";
     exit 1;
 fi
 
 
-response=$(curl -s -u $SONAR_LOGIN:$SONAR_PASSWORD $SONAR_HOST/api/qualitygates/project_status?projectKey=$PROJECT_ORG:$PROJECT_NAME | jq '.projectStatus.status')
+response=$(curl -s -u $PLUGIN_USERNAME:$PLUGIN_PASSWORD $PLUGIN_HOST/api/qualitygates/project_status?projectKey=$PLUGIN_PROJECT_ORGANIZATION:$PLUGIN_PROJECT_NAME | jq '.projectStatus.status')
 
 case "$response" in
   *ERROR*)
